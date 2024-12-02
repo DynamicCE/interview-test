4f56daaeaabbadc9eacd0c1db4ec8c61b796a893
Adınız <erkanmurat0632@gmail.com>
2024-12-02 23:36:41 +0300

    feat: ErrorResponse DTO eklendi

---
+++ interview-test-backend/src/main/java/com/erkan/interview_test_backend/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.erkan.interview_test_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+}
