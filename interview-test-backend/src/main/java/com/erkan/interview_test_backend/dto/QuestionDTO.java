0660bff8c0b25a2aff587d579fb4b5e72c971b96
Adınız <erkanmurat0632@gmail.com>
2024-12-03 01:11:35 +0300

    Update DTO and Entity classes for improved data handling

--- interview-test-backend/src/main/java/com/erkan/interview_test_backend/dto/QuestionDTO.java
+++ interview-test-backend/src/main/java/com/erkan/interview_test_backend/dto/QuestionDTO.java
@@ -2,16 +2,89 @@ package com.erkan.interview_test_backend.dto;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class QuestionDTO {
     private String question;
     private List<String> options;
     private String correctAnswer;
     private String explanation;
+
+    public QuestionDTO() {}
+
+    public QuestionDTO(String question, List<String> options, String correctAnswer,
+            String explanation) {
+        this.question = question;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+        this.explanation = explanation;
+    }
+
+    // Getters
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    // Setters
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    // Builder
+    public static QuestionDTOBuilder builder() {
+        return new QuestionDTOBuilder();
+    }
+
+    public static class QuestionDTOBuilder {
+        private String question;
+        private List<String> options;
+        private String correctAnswer;
+        private String explanation;
+
+        public QuestionDTOBuilder question(String question) {
+            this.question = question;
+            return this;
+        }
+
+        public QuestionDTOBuilder options(List<String> options) {
+            this.options = options;
+            return this;
+        }
+
+        public QuestionDTOBuilder correctAnswer(String correctAnswer) {
+            this.correctAnswer = correctAnswer;
+            return this;
+        }
+
+        public QuestionDTOBuilder explanation(String explanation) {
+            this.explanation = explanation;
+            return this;
+        }
+
+        public QuestionDTO build() {
+            return new QuestionDTO(question, options, correctAnswer, explanation);
+        }
+    }
 }
--- interview-test-backend/src/main/java/com/erkan/interview_test_backend/entity/TestResult.java
+++ interview-test-backend/src/main/java/com/erkan/interview_test_backend/entity/TestResult.java
@@ -14,15 +14,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "test_results")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class TestResult {
 
     @Id
@@ -40,4 +34,99 @@ public class TestResult {
 
     @Enumerated(EnumType.STRING)
     private TestCategory category;
+
+    public TestResult() {}
+
+    public TestResult(Long id, Integer score, List<String> weakTopics, LocalDateTime testDate,
+            TestCategory category) {
+        this.id = id;
+        this.score = score;
+        this.weakTopics = weakTopics;
+        this.testDate = testDate;
+        this.category = category;
+    }
+
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public List<String> getWeakTopics() {
+        return weakTopics;
+    }
+
+    public LocalDateTime getTestDate() {
+        return testDate;
+    }
+
+    public TestCategory getCategory() {
+        return category;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public void setWeakTopics(List<String> weakTopics) {
+        this.weakTopics = weakTopics;
+    }
+
+    public void setTestDate(LocalDateTime testDate) {
+        this.testDate = testDate;
+    }
+
+    public void setCategory(TestCategory category) {
+        this.category = category;
+    }
+
+    // Builder
+    public static TestResultBuilder builder() {
+        return new TestResultBuilder();
+    }
+
+    public static class TestResultBuilder {
+        private Long id;
+        private Integer score;
+        private List<String> weakTopics;
+        private LocalDateTime testDate;
+        private TestCategory category;
+
+        public TestResultBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public TestResultBuilder score(Integer score) {
+            this.score = score;
+            return this;
+        }
+
+        public TestResultBuilder weakTopics(List<String> weakTopics) {
+            this.weakTopics = weakTopics;
+            return this;
+        }
+
+        public TestResultBuilder testDate(LocalDateTime testDate) {
+            this.testDate = testDate;
+            return this;
+        }
+
+        public TestResultBuilder category(TestCategory category) {
+            this.category = category;
+            return this;
+        }
+
+        public TestResult build() {
+            return new TestResult(id, score, weakTopics, testDate, category);
+        }
+    }
 }
