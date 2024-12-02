4c25eca6c41f9c45fbc18fcf64cb7cc0b17a2577
Adınız <erkanmurat0632@gmail.com>
2024-12-03 01:13:50 +0300

    Enhance HuggingFaceResponseDTO with additional response fields

--- interview-test-backend/src/main/java/com/erkan/interview_test_backend/dto/HuggingFaceResponseDTO.java
+++ interview-test-backend/src/main/java/com/erkan/interview_test_backend/dto/HuggingFaceResponseDTO.java
@@ -1,12 +1,39 @@
 package com.erkan.interview_test_backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class HuggingFaceResponseDTO {
     private String generatedText;
+
+    public HuggingFaceResponseDTO() {}
+
+    public HuggingFaceResponseDTO(String generatedText) {
+        this.generatedText = generatedText;
+    }
+
+    // Getters
+    public String getGeneratedText() {
+        return generatedText;
+    }
+
+    // Setters
+    public void setGeneratedText(String generatedText) {
+        this.generatedText = generatedText;
+    }
+
+    // Builder
+    public static HuggingFaceResponseDTOBuilder builder() {
+        return new HuggingFaceResponseDTOBuilder();
+    }
+
+    public static class HuggingFaceResponseDTOBuilder {
+        private String generatedText;
+
+        public HuggingFaceResponseDTOBuilder generatedText(String generatedText) {
+            this.generatedText = generatedText;
+            return this;
+        }
+
+        public HuggingFaceResponseDTO build() {
+            return new HuggingFaceResponseDTO(generatedText);
+        }
+    }
 }
