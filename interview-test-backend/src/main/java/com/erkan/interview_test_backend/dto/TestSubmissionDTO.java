e96236a4fbe1a2b3bb78c539f947a361967ac2f1
Adınız <erkanmurat0632@gmail.com>
2024-12-02 23:36:41 +0300

    feat: Test sonuçlarını göndermek için DTO eklendi

---
+++ interview-test-backend/src/main/java/com/erkan/interview_test_backend/dto/TestSubmissionDTO.java
@@ -0,0 +1,18 @@
+package com.erkan.interview_test_backend.dto;
+
+import java.util.List;
+
+import com.erkan.interview_test_backend.entity.TestCategory;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestSubmissionDTO {
+    private TestCategory category;
+    private List<String> answers;
+    private List<String> weakTopics;
+}
