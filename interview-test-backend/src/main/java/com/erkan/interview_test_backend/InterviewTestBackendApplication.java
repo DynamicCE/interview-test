4b5e722359f503014d05718d7afc6dde263db35b
Adınız <erkanmurat0632@gmail.com>
2024-12-02 23:30:10 +0300

    başladı

---
+++ interview-test-backend/src/main/java/com/erkan/interview_test_backend/InterviewTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.erkan.interview_test_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewTestBackendApplication.class, args);
+	}
+
+}
---
+++ interview-test-backend/src/test/java/com/erkan/interview_test_backend/InterviewTestBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.erkan.interview_test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
